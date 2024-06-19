2e3031e0290b611b4788805355cdd47ebc74c058
Douglas Garcia Rangel <109640187+Douglas4GR@users.noreply.github.com>
2024-06-19 20:46:27 -0300

    inserindo metodo post

--- PortalAdminEmpregados/Controllers/EmpregadosController.cs
+++ PortalAdminEmpregados/Controllers/EmpregadosController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PortalAdminEmpregados.Data;
+using PortalAdminEmpregados.Models;
+using PortalAdminEmpregados.Models.Entidades;
 
 namespace PortalAdminEmpregados.Controllers
 {
@@ -14,6 +16,7 @@ namespace PortalAdminEmpregados.Controllers
         {
             this.dbContext = dbContext;
         }
+
         [HttpGet]
         public IActionResult GetTodosEmpregados()
         {
@@ -21,6 +24,29 @@ namespace PortalAdminEmpregados.Controllers
             var todosEmpregados = dbContext.Empregados.ToList();
             return Ok(todosEmpregados);
             // aqui estamos retornando um status 200 (OK) e a lista de todos os empregados
+        }
+
+        // aqui é necessário a criação de um objeto, que será quem vai receber os dados do empregado, 
+        // para isso usamos DTOs (Data Transfer Object), qu transferem de uma camada para outra
+        [HttpPost]
+        public IActionResult AdicionarEmpregado(AddEmpregadoDto addEmpregadoDto)
+        {
+            // primeiramente devemos mapear o DTO para a entidade (converter o DTO para a entidade)
+            // porque o dbContext que iremos usar apenas aceita entidades
+            var empregadoEntidade = new Empregado()
+            {
+                Nome = addEmpregadoDto.Nome,
+                Email = addEmpregadoDto.Email,
+                Telefone = addEmpregadoDto.Telefone,
+                CPF = addEmpregadoDto.CPF,
+                Salario = addEmpregadoDto.Salario
+            };
+
+            dbContext.Empregados.Add(empregadoEntidade);
+            dbContext.SaveChanges();
+
+            return Ok(empregadoEntidade);
+            // aqui estamos retornando um status 200 (OK) e o empregado que acabamos de adicionar
         }
     }
 }
---
+++ PortalAdminEmpregados/Models/AddEmpregadoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PortalAdminEmpregados.Models
+{
+    public class AddEmpregadoDto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string CPF { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
