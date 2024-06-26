3ea2857d375f3a3193e7f7248310b908cfe71cf3
Douglas Garcia Rangel <109640187+Douglas4GR@users.noreply.github.com>
2024-06-26 20:01:58 -0300

    inserindo projeto da view

---
+++ PortalEmpregadosView/Program.cs
@@ -0,0 +1,31 @@
+using PortalEmpregadosView.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient("PortalAdminEmpregados", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5201/");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
